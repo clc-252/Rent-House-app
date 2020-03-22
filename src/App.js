561315e854027e738550aa9ffaf6ea3ccbec3c87
蔡丽纯 <12345.qq.com>
2020-03-22 17:16:28 +0800

    04-路由和tabbar相结合，实现路由改变tabbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 // import Tabbar from './demo/Tabbar'
 // 引入路由
-import {HashRouter as Router,Route,Link} from 'react-router-dom';
+import {HashRouter as Router,Route,Redirect} from 'react-router-dom';
 
 import Home from './pages/home'
-import Found from './pages/found'
-import News from './pages/news'
-import My from './pages/my'
+// 地图找房
+import MapFound from './pages/mapFound'
+// 城市选择
+import CitySelect from './pages/citySelect'
+
 function App() {
   return (
     <div className="App">
       {/* <Tabbar></Tabbar> */}
       <Router>
-        <nav>
-          <Link to="/">首页</Link>
-          <Link to="/found">找房</Link>
-          <Link to="/news">资讯</Link>
-          <Link to="/my">我的</Link>
-        </nav>
-        <section>
-          <Route exact component={Home} path="/"></Route>
-          <Route exact component={Found} path="/found"></Route>
-          <Route exact component={News} path="/news"></Route>
-          <Route exact component={My} path="/my"></Route>
-        </section>
+          <Route component={Home} path="/home"></Route>
+          <Route path="/" exact>
+            <Redirect to="/home"></Redirect>
+          </Route>
+          <Route component={MapFound} path="/mapFound" exact></Route>
+          <Route component={CitySelect} path="/citySelect" exact></Route>
       </Router>
     </div>
   );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,88 @@
-import React, { Component,Fragment } from 'react';
-class index extends Component {
-    render() { 
-        return ( 
-            <Fragment>首页</Fragment>
-         );
-    }
+import React from 'react'
+import { TabBar } from 'antd-mobile';
+
+import {Route} from 'react-router-dom';
+
+// 首页
+import Index from '../index'
+// 找房
+import Found from '../found'
+// 资讯
+import News from '../news'
+// 我的
+import My from '../my'
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab'
+    };
+  }
+
+  renderContent(pageText) {
+    return (
+      <div>
+          <i className="iconfont icon-edit"></i>
+          {pageText}
+      </div>
+    );
+  }
+// 修改判断条件
+  render() {
+      const {location,history}=this.props
+    return (
+      <div style={ { position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#21b97a"
+          barTintColor="white"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Home"
+            icon={ <i className="iconfont icon-ind"></i> }
+            selectedIcon={<i className="iconfont icon-ind"></i>}
+            selected={location.pathname === '/home/index'}
+            onPress={() => history.push('/home/index')}
+            data-seed="logId"
+          >
+            <Route component={Index} path="/home/index"></Route>
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<i className="iconfont icon-findHouse"></i>}
+            selectedIcon={<i className="iconfont icon-findHouse"></i>}
+            title="找房"
+            key="found"
+            selected={location.pathname === '/home/found'}
+            onPress={() => history.push('/home/found')}
+            data-seed="logId1"
+          >
+            <Route component={Found} path="/home/found"></Route>
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<i className="iconfont icon-infom"></i>}
+            selectedIcon={<i className="iconfont icon-infom"></i>}
+            title="资讯"
+            key="news"
+            selected={location.pathname === '/home/news'}
+            onPress={() => history.push('/home/news')}
+          >
+            <Route component={News} path="/home/news"></Route>
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<i className="iconfont icon-my"></i>}
+            selectedIcon={<i className="iconfont icon-my"></i>}
+            title="我的"
+            key="my"
+            selected={location.pathname === '/home/my'}
+            onPress={() => history.push('/home/my')}
+          >
+            <Route component={My} path="/home/my"></Route>
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
 }
- 
-export default index;
+
+export default Home
