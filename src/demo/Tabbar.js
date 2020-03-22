0ab6d08c490942ffeaa6a7e58b06accd814c7b8b
蔡丽纯 <12345.qq.com>
2020-03-22 10:33:54 +0800

    03-添加tabbar

---
+++ src/demo/Tabbar.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { TabBar } from 'antd-mobile';
+
+class TabBarExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab'
+    };
+  }
+
+  renderContent(pageText) {
+    return (
+      <div>
+          <i className="iconfont icon-edit"></i>
+          {pageText}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div style={ { position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#21b97a"
+          barTintColor="white"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Home"
+            icon={ <i className="iconfont icon-ind"></i> }
+            selectedIcon={<i className="iconfont icon-ind"></i>}
+            selected={this.state.selectedTab === 'blueTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            {this.renderContent('Life')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<i className="iconfont icon-findHouse"></i>}
+            selectedIcon={<i className="iconfont icon-findHouse"></i>}
+            title="找房"
+            key="found"
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId1"
+          >
+            {this.renderContent('Koubei')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<i className="iconfont icon-infom"></i>}
+            selectedIcon={<i className="iconfont icon-infom"></i>}
+            title="资讯"
+            key="news"
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+            {this.renderContent('Friend')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<i className="iconfont icon-my"></i>}
+            selectedIcon={<i className="iconfont icon-my"></i>}
+            title="我的"
+            key="my"
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab',
+              });
+            }}
+          >
+            {this.renderContent('My')}
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
+}
+
+export default TabBarExample
