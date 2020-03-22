1aa6700fb07e4e9ec4d7774e3c0898429c7ce924
蔡丽纯 <12345.qq.com>
2020-03-22 09:21:51 +0800

    02-引入ui框架

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 function App() {
   return (
     <div className="App">
-      123
+      {/* WingBlank：两个按钮之间的左右间隙 */}
+      <WingBlank>
+        <Button type="primary">primary</Button>
+        {/* WhiteSpace：两个按钮之间的空隙（上下） */}
+        <WhiteSpace />
+        <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
+      </WingBlank>
     </div>
   );
 }
