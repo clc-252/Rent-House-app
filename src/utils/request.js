c4d016f04990e995094ad03cacbba3ce8ff64747
蔡丽纯 <12345.qq.com>
2020-03-23 10:30:15 +0800

    11-解决同时发送多个请求的加载中提示的关闭问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,32 +9,45 @@
 // 引入
 import axios from 'axios'
 // 引入Toast轻提示
-import { Toast } from 'antd-mobile';
+import {
+  Toast
+} from 'antd-mobile';
 
-export const baseURL='http://157.122.54.189:9060';
-axios.defaults.baseURL=baseURL;
+export const baseURL = 'http://157.122.54.189:9060';
+axios.defaults.baseURL = baseURL;
+
+// 定义一个变量记录发送请求的次数
+let AjaxTimes = 0;
 
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
-    // 提示加载中
-    Toast.loading('加载中...', 0);
-    return config;
-  }, function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  });
+  // 在发送请求之前做些什么
+  // 每发送前一个请求，AjaxTimes+1
+  AjaxTimes++
+  // 提示加载中
+  Toast.loading('加载中...', 0);
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
 
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
-    // 对响应数据做点什么
+  // 对响应数据做点什么
+  // 请求回来AjaxTimes—1
+  AjaxTimes--
+  // 当最后一个回来的时候
+  if (AjaxTimes === 0) {
     // 加载中提示隐藏
     Toast.hide()
-    return response;
-  }, function (error) {
-    // 对响应错误做点什么
-    return Promise.reject(error);
-  });
+  }
+
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
 
 // 暴露
 export default axios;
